4d189eb296d3946d316a1459f6341af4e7fd4f3d
juaninarvaja <juaninarvaja@gmail.com>
2019-04-16 21:59:48 -0300

    Add files via upload

---
+++ TP/UTN (1).sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-04-2019 a las 02:58:15
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `UTN`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envios`
+--
+
+CREATE TABLE `envios` (
+  `numero` int(11) NOT NULL,
+  `pNumero` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `envios`
+--
+
+INSERT INTO `envios` (`numero`, `pNumero`, `cantidad`) VALUES
+(100, 1, 500),
+(100, 2, 1500),
+(100, 3, 100),
+(101, 2, 55),
+(101, 3, 225),
+(102, 1, 600),
+(102, 3, 300);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `pNumero` int(11) NOT NULL,
+  `pNombre` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `precio` float NOT NULL,
+  `tamaño` varchar(20) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`pNumero`, `pNombre`, `precio`, `tamaño`) VALUES
+(1, 'Caramelos', 1.5, 'Chico'),
+(2, 'Cigarrillos', 45.89, 'Mediano'),
+(3, 'Gaseosa', 15.8, 'Grande');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provedores`
+--
+
+CREATE TABLE `provedores` (
+  `numero` int(11) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `domicilio` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `localidad` varchar(80) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `provedores`
+--
+
+INSERT INTO `provedores` (`numero`, `nombre`, `domicilio`, `localidad`) VALUES
+(100, 'Perez', 'Perón 876', 'Quilmes'),
+(101, 'Gimenez', 'Mitre 750', 'Avellaneda'),
+(102, 'Aguirre', 'Boedo 634', 'Bernal');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `envios`
+--
+ALTER TABLE `envios`
+  ADD PRIMARY KEY (`numero`,`pNumero`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`pNumero`);
+
+--
+-- Indices de la tabla `provedores`
+--
+ALTER TABLE `provedores`
+  ADD PRIMARY KEY (`numero`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `pNumero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `provedores`
+--
+ALTER TABLE `provedores`
+  MODIFY `numero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
